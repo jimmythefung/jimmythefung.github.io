0c219d4dbdd408fe88bdc96d7237b35710a00536
Jimmy Fung <jimmythefung@yahoo.com>
2023-08-13 17:27:30 -0400

    using nextjs page router

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-    return (
-        <main>
-            <div>
-                <p>Hello World Again!</p>
-            </div>
-        </main>
-    );
-}
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Jimmy Fung</title>
+                <meta name="description" content="Jimmy's Github Pages" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                    user-scalable="yes"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main>
+                <h1>Jimmy's Github Pages</h1>
+            </main>
+        </>
+    );
+}
